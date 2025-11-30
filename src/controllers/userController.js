2cba81c68846166ede755d167e31ebd279a29e07
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-11-30 01:56:21 -0300

    feat:adiciona rotas de usuários

---
+++ src/controllers/userController.js
@@ -0,0 +1,14 @@
+export const createUser = (req, res) => {
+
+    res.status(200).send({message: "Parece que deu certo!!!"})
+}
+
+export const getAllteUser = (req, res) => {
+
+    res.status(200).send({message: "Parece que deu certo!!!"})
+}
+
+export const deleteUser = (req, res) => {
+
+    res.status(200).send({message: "Parece que deu certo!!!"})
+}
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,10 @@
 import express from 'express'
+import {createUser, getAllUsers, deleteUser} from '../controller/userController.js'
 
 const router = express.Router()
+
+router.post('/cadastro', createUser)
+router.get('/todes', getAllUsers)
+router.delete('/deletar', deleteUser)
+
+export default router
